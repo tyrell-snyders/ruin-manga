4732c046d94797000231f51a4d507273c70b0883
tyrell-snyders <tyrell.snyders@gmail.com>
2023-09-26 23:40:02 +0200

    Added DB connection to mysql

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,13 @@ import envSchema from 'env-schema'
 import { Type, Static } from '@sinclair/typebox'
 
 const schema = Type.Object({
-    DB_URL: Type.String(),
-    
+    DB_HOST: Type.String({
+        default: 'localhost'
+    }),
+    DB_USER: Type.String({
+        default: 'root'
+    }),
+    DB_NAME: Type.String()
 })
 
 type Env = Static<typeof schema>
@@ -12,4 +17,3 @@ export const config = envSchema<Env>({
     schema,
     dotenv: true
 })
-
