4d7636d30e59d4ce3520827de270e0b941b0717e
tyrell-snyders <tyrell.snyders@gmail.com>
2023-09-26 23:27:36 +0200

    Installed Mysql driver and added a config file

---
+++ src/utils/config.ts
@@ -0,0 +1,15 @@
+import envSchema from 'env-schema'
+import { Type, Static } from '@sinclair/typebox'
+
+const schema = Type.Object({
+    DB_URL: Type.String(),
+    
+})
+
+type Env = Static<typeof schema>
+
+export const config = envSchema<Env>({
+    schema,
+    dotenv: true
+})
+
