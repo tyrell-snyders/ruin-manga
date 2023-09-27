d6ebb4efb8b16f6d45669df215dcb5c5dcb4257c
tyrell-snyders <tyrell.snyders@gmail.com>
2023-09-27 14:22:12 +0200

    feat: added global context

---
+++ src/context/index.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { usePathname, useRouter } from 'next/navigation'
+import { createContext, useState, useEffect } from 'react'
+
+type GlobalContextValue = {
+    showNavModal: boolean;
+    setShowNavModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const GlobalContext = createContext<GlobalContextValue | null>(null)
+
+export default function GlobalState({ children } : {children: React.ReactNode}) {
+    //Hooks
+    const [showNavModal, setShowNavModal] = useState(false)
+
+    const router = useRouter()
+    const pathName = usePathname()
+
+    return (
+        <>
+            <GlobalContext.Provider value={{
+                showNavModal, setShowNavModal, 
+            }}>
+                {children}
+            </GlobalContext.Provider>
+        </>
+    )
+}
