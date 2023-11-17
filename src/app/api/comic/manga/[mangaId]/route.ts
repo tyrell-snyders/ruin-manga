7c692d72e8ffb1aed5b58d748d9332665010fad9
tyrell-snyders <tyrell.snyders@gmail.com>
2023-11-17 16:09:58 +0200

    feat: manga data fetch

---
+++ src/app/api/comic/manga/[mangaId]/route.ts
@@ -0,0 +1,42 @@
+import { logger } from "@/utils/logger"
+import { NextResponse, NextRequest } from "next/server"
+
+const url = 'http://localhost:8081/api/v1/comic/manga'
+
+export const dynamic = 'force-dynamic'
+
+export const GET = async (req: NextRequest, route : { params: {mangaId: string} }) => {
+    if (req.method === 'GET') {
+        try {
+            const mangaId: string = route.params.mangaId
+            const response = await fetch(`${url}/${mangaId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            if (response.status === 200) {
+                const data = await response.json()
+                return NextResponse.json({
+                    data,
+                    message: '',
+                    success: true
+                })
+            } else 
+                return new NextResponse(response.statusText, {
+                    status: response.status
+                })
+        } catch (e) {
+            if (e instanceof Error) {
+                logger.error(`${e.message}`)
+                return new NextResponse('Internal Server Error', {
+                    status: 500
+                })
+            }
+        }
+    } else
+        return new NextResponse('Not Allowed', {
+            status: 405
+        })
+}
