8b792b10f213238ffe90834941f4ac94c290b47f
tyrell-snyders <tyrell.snyders@gmail.com>
2024-02-22 15:20:24 +0200

    feat: navigate to grid item pages

--- src/app/Account/page.tsx
+++ src/app/Account/page.tsx
@@ -3,11 +3,12 @@
 import { useState, useEffect, useContext } from "react"
 import { logger } from "@/utils/logger"
 import { GlobalContext } from "@/context"
+import { useRouter } from "next/navigation"
 
 const styles = {
     container: `flex flex-col items-center justify-center mt-20`,
     section: `grid grid-cols-1 sm:grid-cols-3 gap-20 mt-20`,
-    gridItem: `flex flex-col items-center justify-center border border-gray-200 rounded-lg p-4`,
+    gridItem: `flex flex-col items-center justify-center border border-gray-200 rounded-lg p-4 cursor-pointer`,
     h1: `text-4xl font-bold`,
     details: `flex flex-col mt-4 border border-gray-200 rounded-lg p-4`,
     h2: `text-xl font-bold`,
@@ -22,40 +23,41 @@ export default function AccoutPage() {
         </div>
     }
 
-    const { user, isAuth } = context
+    const { loading, user, isAuth, favourites } = context
 
-    if (isAuth) {
+    const router = useRouter()
+
         return (
-            <div className={styles.container}>
-                <h1 className={styles.h1}>Welcome, {user.username}</h1>
-                <section className={styles.section}>
-                    {/* Friends */}
-                    <div className={styles.gridItem}>
-                        <h2 className={styles.h2}>Friends</h2>
-                        {/* Friends count */}
-                        {13}
-                    </div>
-                    {/* Discussions */}
-                    <div className={styles.gridItem}>
-                        <h2 className={styles.h2}>Discussions</h2>
-                        {/* Discussions count */}
-                        {10}
-                    </div>
-                    {/* Favourites */}
-                    <div className={styles.gridItem}>
-                        <h2 className={styles.h2}>Favourites</h2>
-                        {/* Favourites count */}
-                        {5}
-                    </div>
-                </section>
-            </div>
+            <>
+                { !loading && isAuth && favourites && favourites.favourites.length ? 
+                    (
+                        <div className={styles.container}>
+                            <h1 className={styles.h1}>Welcome, {user.username}</h1>
+                            <section className={styles.section}>
+                                {/* Friends */}
+                                <div className={styles.gridItem}>
+                                    <h2 className={styles.h2}>Friends</h2>
+                                    {/* Friends count */}
+                                    {13}
+                                </div>
+                                {/* Discussions */}
+                                <div className={styles.gridItem}>
+                                    <h2 className={styles.h2}>Discussions</h2>
+                                    {/* Discussions count */}
+                                    {10}
+                                </div>
+                                {/* Favourites */}
+                                <div className={styles.gridItem} onClick={() => router.push('/Favourites')}>
+                                    <h2 className={styles.h2}>Favourites</h2>
+                                    {/* Favourites count */}
+                                    {favourites?.favourites.length}
+                                </div>
+                            </section>
+                        </div>
+                ) : ( 
+                    <div className={styles.container}>Loading...</div>
+                )
+            }
+            </>
         )
-    } else {
-        return (
-            <div className="flex flex-col items-center justify-center mt-20">
-                <h1>You are not authorized to this page!!!</h1>
-                <h1>Please Sign in</h1>
-            </div>
-        )
-    }
 }
